f42efef250002034534aa67c439cb42f598cb08c
Andy Hume <ahume@twitter.com>
2015-02-13 18:21:33 +0000

    Initial commit out of TweetDeck codebase

---
+++ README.md
@@ -0,0 +1,50 @@
+# flight-with-observe-state
+
+[![Build Status](https://secure.travis-ci.org/flightjs/flight-with-observe-state.svg)](http://travis-ci.org/flightjs/flight-with-observe-state)
+
+A [Flight](https://github.com/flightjs/flight) mixin for filtering, transforming, and subscribing to observable state.
+
+## Installation
+
+```bash
+bower install --save flight-with-observable-state
+```
+
+## Example
+
+
+
+
+## API
+
+
+## Development
+
+Development of this component requires [Bower](http://bower.io) to be globally
+installed:
+
+```bash
+npm install -g bower
+```
+
+Then install the Node.js and client-side dependencies by running the following
+commands in the repo's root directory.
+
+```bash
+npm install & bower install
+```
+
+To continuously run the tests in Chrome during development, just run:
+
+```bash
+npm run watch-test
+```
+
+## Contributing to this project
+
+Anyone and everyone is welcome to contribute. Please take a moment to
+review the [guidelines for contributing](CONTRIBUTING.md).
+
+* [Bug reports](CONTRIBUTING.md#bugs)
+* [Feature requests](CONTRIBUTING.md#features)
+* [Pull requests](CONTRIBUTING.md#pull-requests)
---
+++ test/spec/with-observe-state.spec.js
@@ -0,0 +1,91 @@
+define(function (require) {
+    'use strict';
+
+    var Rx = require('rxjs');
+
+    describeMixin('lib/with-observe-state', function () {
+
+        var observable;
+        var observableState;
+
+        beforeEach(function () {
+            observable = new Rx.BehaviorSubject(1);
+            observableState = new Rx.BehaviorSubject({
+                count: 1,
+                active: false
+            });
+            this.setupComponent();
+        });
+
+        it('should observe changed values', function (done) {
+            this.component.observe(observable).subscribeOnNext(function (value) {
+                if (value === 2) {
+                    done();
+                }
+            });
+            observable.onNext(2);
+        });
+
+        it ('should dispose of observables on teardown', function () {
+            this.component.observe(observable);
+
+            var called = 0;
+            this.component.observe(observable).subscribeOnNext(function (value) {
+                called = called + 1;
+            });
+            expect(called).toBe(1);
+
+            observable.onNext('still subscribed');
+            expect(called).toBe(2);
+
+            // Teardown the component and check handler is not called again.
+            this.component.teardown();
+            observable.onNext('not subscribed');
+            expect(called).toBe(2);
+
+        });
+
+        describe('observable state', function () {
+            it('should observe changed state values', function (done) {
+                this.component.observeState(observableState).subscribeOnNext(function (state) {
+                    if (state.count === 2) {
+                        done();
+                    }
+                });
+                observableState.onNext({
+                    count: 2
+                });
+            });
+
+            it('should not observe unchanged state values', function () {
+                var called = 0;
+                this.component.observeState(observableState, ['count']).subscribe(function (state) {
+                    called = called + 1;
+                });
+                expect(called).toBe(1);
+
+                // Push an unchanged/duplicate value into the stream.
+                observableState.onNext({
+                    count: 1
+                });
+                expect(called).toBe(1);
+            });
+
+            it('should only return state for requested keys', function (done) {
+                this.component.observeState(observableState, ['count']).subscribe(function (state) {
+                    expect(state.alive).toBe(undefined);
+                    expect(state.count).toBe(1);
+                    done();
+                });
+            });
+
+            it('should return all state keys if none are passed in', function (done) {
+                this.component.observeState(observableState).subscribe(function (state) {
+                    expect(state.active).toBe(false);
+                    expect(state.count).toBe(1);
+                    done();
+                });
+            });
+        });
+    });
+});
